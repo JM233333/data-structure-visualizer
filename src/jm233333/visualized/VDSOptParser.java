25b7379d521c47125b56671c03d430aa0c2751be
JM233333 <26466398@163.com>
2020-09-09 23:28:45 +0800

    upd

---
+++ src/jm233333/visualized/VDSOptParser.java
@@ -0,0 +1,17 @@
+package jm233333.visualized;
+
+import java.util.ArrayList;
+
+import jm233333.util.Pair;
+
+public class VDSOptParser {
+
+    public static Pair<String, ArrayList<String>> parse(String str) {
+        //
+        Pair<String, ArrayList<String>> operation = new Pair<>();
+        //
+        return operation;
+    }
+
+    private VDSOptParser() {}
+}
